0e618448dc348f7b8d6c1d5919a7a95712fa761e
omriraccah <omriraccah@gmail.com>
2022-04-25 16:20:00 -0400

    lots of shit

---
+++ Download_Data.py
@@ -0,0 +1,33 @@
+"""#########################################################################
+This script:
+Reads the processed qualtrics CSV, and based on its entries downloads the relevant raw data into the raw data folder
+Note: for this to work, the AWS CLI must be installed, otherwise the downloading must be done manually.
+Also Note: This may take a long time. (hours)
+#########################################################################"""
+
+import os
+import pandas as pd
+raw_data_dir = '/Users/omriraccah/Documents/Projects/Music_ScaleAbstraction/Sequence_Completion_analysis/data/'
+qualtrics_path = '/Users/omriraccah/Documents/Projects/Music_ScaleAbstraction/Sequence_Completion_analysis/survey/Sequence_Completion_Apr_2022.csv'
+
+def download_data(qualtrics_path,raw_data_dir):
+    # Loads the processed qualtrics csv we created in 1_Preprocess_Qualtrics_data.py
+    qualtrics = pd.read_csv(qualtrics_path)  # path of qualtrics survey with all relevant responses
+
+    # extract all subjects (task set IDs) from qualtrics data
+    subjects = qualtrics['sub'].tolist()[2:]
+
+    # Get folders already in folder:
+    dirs = [dir for dir in os.listdir(raw_data_dir)]
+
+    # Download each subject's task set
+    for SubjectID in subjects:
+        if(SubjectID in dirs):
+            print("Subject {} already exists, moving to the next subject.".format(SubjectID))
+            continue
+        print(raw_data_dir)
+        print("Downloading data for subject: ", SubjectID)
+        os.system('aws s3 cp s3://cadseg/{0} {1}{0} --exclude="*audio*" --exclude="*images*" --recursive'
+                  .format(SubjectID,raw_data_dir))  # Runs using the AWS CLI
+
+download_data(qualtrics_path,raw_data_dir)
---
+++ plot_script_Omris_version.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from stat_funcs import *
+
+data_dir = './analyzed/'
+
+data_completion = pd.read_csv(data_dir + 'seq_results.csv')
+reps = 10000
+
+# H: analyze only nonexcluded participants
+data_completion_AC = data_completion[data_completion.excluded == False]
+data_completion_AC = data_completion_AC[data_completion_AC['d-prime'] > 0]
+
+data_completion_AC["musical_training"].replace({"9+": "9"}, inplace=True)
+data_completion_AC['musical_training'] = data_completion_AC['musical_training'].astype(int)
+
+# H: Remove subjects with more than 0 yrs of musical training and less than 5 yrs
+data_completion_AC_Music = data_completion_AC.loc[(data_completion_AC['musical_training']==0) | (data_completion_AC['musical_training']>4)]
+
+data_completion_AC_Music['musician'] = False
+data_completion_AC_Music['musician'] = data_completion_AC_Music['musical_training'] >= 5
+
+data_completion_AC_NonMusicians = data_completion_AC.loc[data_completion_AC['musical_training']==0]
+data_completion_AC_Musicians = data_completion_AC.loc[data_completion_AC['musical_training']>4]
+
+# H: plot diatonic versus chromatic d-prime (entire cohort)
+sns.set_context("talk")
+d_prime_AC_reform = pd.melt(data_completion_AC, value_vars=['diatonic_d-prime', 'chromatic_d-prime'])
+ax = sns.boxplot(x="variable", y="value", data=d_prime_AC_reform, saturation=.5)
+sns.despine()
+sns.set_context("talk")
+plt.show()
+delta_paired(X, Y, reps)
+
+# H: plot diatonic versus chromatic d-prime (musicians vs. non-musicians)
+fig, ax = plt.subplots()
+sns.set_style("ticks")
+sns.set_context("talk")
+data_confab_AC_reform_FA_rate = pd.melt(data_completion_AC_Music, id_vars=['musician'], value_vars=['diatonic_d-prime', 'chromatic_d-prime'])
+sns.barplot(x="musician", y="value", hue="variable", data=data_confab_AC_reform_FA_rate, ax=ax, color=".9")
+sns.stripplot(x="musician", y="value", hue="variable", data=data_confab_AC_reform_FA_rate, ax=ax, dodge=True, palette="Set2", size=13,
+              marker=".", edgecolor="gray", alpha=.15)
+#ax.set_ylim(0, .9)
+sns.despine()
+plt.legend([],[], frameon=False)
+plt.show()
+
+delta_paired(data_completion_AC['diatonic_d-prime'], data_completion_AC['chromatic_d-prime'], 10000)
+delta_paired(data_completion_AC_Musicians['diatonic_d-prime'], data_completion_AC_Musicians['chromatic_d-prime'], 10000)
+delta_paired(data_completion_AC_NonMusicians['diatonic_d-prime'], data_completion_AC_NonMusicians['chromatic_d-prime'], 10000)
+
+# H: plot diatonic versus chromatic HITS (musicians vs. non-musicians)
+fig, ax = plt.subplots()
+sns.set_style("ticks")
+sns.set_context("talk")
+data_confab_AC_reform_FA_rate = pd.melt(data_completion_AC_Music, id_vars=['musician'], value_vars=['diatonic_d-prime', 'chromatic_d-prime'])
+sns.barplot(x="musician", y="value", hue="variable", data=data_confab_AC_reform_FA_rate, ax=ax, color=".9")
+sns.stripplot(x="musician", y="value", hue="variable", data=data_confab_AC_reform_FA_rate, ax=ax, dodge=True, palette="Set2", size=13,
+              marker=".", edgecolor="gray", alpha=.15)
+#ax.set_ylim(0, .9)
+sns.despine()
+plt.legend([],[], frameon=False)
+plt.show()
+
+delta_paired(data_completion_AC['diatonic_d-prime'], data_completion_AC['chromatic_d-prime'], 10000)
+delta_paired(data_completion_AC_Musicians['diatonic_d-prime'], data_completion_AC_Musicians['chromatic_d-prime'], 10000)
+delta_paired(data_completion_AC_NonMusicians['diatonic_d-prime'], data_completion_AC_NonMusicians['chromatic_d-prime'], 10000)
